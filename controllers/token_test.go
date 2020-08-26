be6c699e5b629b5f16ac926db23027210abd415d
Sondre Naustdal <sondre.naustdal@gmail.com>
2020-08-26 10:50:48 +0200

    Add context mock, and test token endpoint

--- controllers/token_test.go
+++ controllers/token_test.go
@@ -2,14 +2,19 @@ package controllers
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
+	"net/http"
 	"reflect"
 	"sort"
 	"testing"
+
+	"github.com/equinor/no-factor-auth/tests"
+	"github.com/golang/mock/gomock"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestShouldReturnParseError(t *testing.T) {
-	jsonString := []byte ("malformed json")
+	jsonString := []byte("malformed json")
 
 	_, err := ParseExtraClaims(jsonString)
 	assert.Error(t, err, "Malformed json should return error")
@@ -20,7 +25,7 @@ func TestParseJson(t *testing.T) {
 	oidValue := "b213-61024b63a7ea"
 	arrValue := "some-value"
 
-	jsonString := []byte (`{"oid":"` + oidValue + `", "arr_elem_key":["` + arrValue + `","foo"]}`)
+	jsonString := []byte(`{"oid":"` + oidValue + `", "arr_elem_key":["` + arrValue + `","foo"]}`)
 
 	result, err := ParseExtraClaims(jsonString)
 
@@ -49,4 +54,47 @@ func extractArrayElement(result map[string]interface{}) []string {
 	}
 
 	return items
+}
+
+func TestToken(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	ctx := tests.NewMockContext(ctrl)
+	ctx.EXPECT().
+		QueryParam("redirect_uri").
+		Return("http://example.com")
+	ctx.EXPECT().
+		QueryParam("client_id").
+		Return("1234")
+	ctx.EXPECT().
+		QueryParam("grant_type").
+		Return("client_credential")
+
+	ctx.EXPECT().
+		QueryParam("code").
+		Return("code")
+
+	ctx.EXPECT().
+		QueryParam("client_secret").
+		Return("secret")
+
+	ctx.EXPECT().
+		QueryParam("extra_claims").
+		Return("")
+
+	ctx.EXPECT().
+		Request().
+		Return(&http.Request{Host: "http://localhost"})
+
+	ctx.EXPECT().
+		JSON(http.StatusOK, gomock.Any()).
+		Return(nil)
+
+	t.Run("On correct query params the token should be received in a json struct", func(t *testing.T) {
+		if err := Token(ctx); err != nil {
+			t.Errorf("Token() error = %v", err)
+		}
+	})
+
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/equinor/no-factor-auth
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/golang/mock v1.4.4
 	github.com/joho/godotenv v1.3.0
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/labstack/gommon v0.3.0
 	github.com/mattn/go-colorable v0.1.7 // indirect
